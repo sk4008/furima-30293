3081c0f367a2cad73df2d9772e332fa6a8658745
sk4008 <73086120+sk4008@users.noreply.github.com>
2021-01-06 17:23:46 +0900

    deviseのインストール

--- db/migrate/20210104113953_create_users.rb
+++ db/migrate/20210104113953_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :nickname
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.string :email
+      t.string :encrypted_password
+      t.date :date
       t.timestamps
     end
   end
--- db/migrate/20210106045550_create_purchases.rb
+++ db/migrate/20210106045550_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.references :user
+      t.references :item
       t.timestamps
     end
   end
